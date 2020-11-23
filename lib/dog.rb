80bca4dc25ea7cf3b97ff2b4c8f8c6d623ea589a
Nico Bazzoni <you@example.com>
2020-11-23 12:45:05 -0500

    commit tree 567eb17e55da36650331b3e18fd6c7d9276a0fe3

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    def bark
+      puts "Woof!"
+    end
+  end
+
+  class Dog 
+    def sit
+         puts "The Dog is sitting"
+        end
+end
+
+class Dog 
+    def The_Dog_is_sitting
+        puts "The Dog is sitting"
+    end 
+end 
